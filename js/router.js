f6d421e5f97848c8a8faae3f1ec96af610bf6687
DummyDevPro <htun@osfl.ac.jp>
2023-01-27 11:46:40 +0900

    remove log

--- js/router.js
+++ js/router.js
@@ -1,8 +1,5 @@
 // Check User Information in browser storage 
 if (true) {
-    console.log("if => true");
-    console.log(window.location.pathname);
-
     let baseUrl = window.location.origin;
     // For GitHub
     if (baseUrl.includes("github.io")) {
@@ -12,19 +9,13 @@ if (true) {
     switch (window.location.pathname) {
         // for github
         case '/AttendanceSystem/':
-            window.location.href = baseUrl + '/pages/login.html';
+            window.location.href = baseUrl + 'pages/login.html';
             break;
 
         // for localhost
         case '/index.html':
         case '/':
             window.location.href = baseUrl + '/pages/login.html';
-            break;
-
-        // for error 
-        case 404:
-        default:
-            window.location.href = baseUrl + '/error-pages/404.html';
             break;
     }
 }
